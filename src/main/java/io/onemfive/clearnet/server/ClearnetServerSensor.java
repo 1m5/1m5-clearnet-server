c8fb868c46c9a07d77693e6bd501a05c71d8f92f
objectorange <objectorange@protonmail.com>
2018-11-17 23:50:05 -0500

    support a method for registering multiple servers with handlers configured by inbound properties.

--- src/main/java/io/onemfive/clearnet/server/ClearnetServerSensor.java
+++ src/main/java/io/onemfive/clearnet/server/ClearnetServerSensor.java
@@ -1,89 +1,56 @@
 package io.onemfive.clearnet.server;
 
 import io.onemfive.core.Config;
+import io.onemfive.core.util.SystemVersion;
 import io.onemfive.sensors.BaseSensor;
-import io.onemfive.sensors.SensorsService;
-import io.onemfive.data.Message;
-import io.onemfive.data.util.DLC;
-import io.onemfive.data.DocumentMessage;
+import io.onemfive.sensors.SensorManager;
 import io.onemfive.data.Envelope;
-import io.onemfive.data.util.Multipart;
-import okhttp3.*;
+import org.eclipse.jetty.server.Handler;
 import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.server.handler.ContextHandler;
 
-import javax.net.ssl.*;
 import java.io.IOException;
 import java.net.InetSocketAddress;
-import java.net.Proxy;
-import java.net.URL;
-import java.nio.ByteBuffer;
 import java.util.*;
 import java.util.logging.Logger;
 
 /**
- * TODO: Add Description
+ * Sets up HTTP server listeners.
+ * Only localhost (127.0.0.1) is supported.
  *
  * @author objectorange
  */
-public class ClearnetServerSensor extends BaseSensor {
+public final class ClearnetServerSensor extends BaseSensor {
+
+    public static final String HANDLER_ID = "1m5.sensors.clearnet.server.handler.id";
+
+    /**
+     * Configuration of Servers in the form name, port, launch on start, concrete implementation of io.onemfive.clearnet.server.AsynchronousHandler|n,...}
+     */
+    public static final String SERVERS_CONFIG = "1m5.sensors.clearnet.server.config";
 
     private static final Logger LOG = Logger.getLogger(ClearnetServerSensor.class.getName());
 
-    public static final String PROP_HTTP_SERVER = "1m5.sensors.clearnet.http.server"; // true | false
-    public static final String PROP_HTTP_SERVER_IP = "1m5.sensors.clearnet.http.server.host"; // ipv4 | ipv6 address
-    public static final String PROP_HTTP_SERVER_PORT = "1m5.sensors.clearnet.http.server.port"; // integer
-    public static final String PROP_HTTP_SERVER_PATH = "1m5.sensors.clearnet.http.server.path"; // path
+    private boolean isTest = false;
 
-    protected static final Set<String> trustedHosts = new HashSet<>();
+    private final List<Server> servers = new ArrayList<>();
+    private final Map<Byte,AsynchronousHandler> handlers = new HashMap<>();
+    private Byte nextHandlerId = 0;
 
-    protected static final HostnameVerifier hostnameVerifier = new HostnameVerifier() {
+    private Properties properties;
 
-        @Override
-        public boolean verify(String hostname, SSLSession session) {
-            return true;
-        }
-    };
+    public ClearnetServerSensor(SensorManager sensorManager, Envelope.Sensitivity sensitivity, Integer priority) {
+        super(sensorManager, sensitivity, priority);
+    }
 
-    protected X509TrustManager x509TrustManager = new X509TrustManager() {
-
-        public java.security.cert.X509Certificate[] getAcceptedIssuers() {
-            return new java.security.cert.X509Certificate[]{};
-        }
-
-        public void checkClientTrusted(java.security.cert.X509Certificate[] certs, String authType) {
-        }
-
-        public void checkServerTrusted(java.security.cert.X509Certificate[] certs, String authType) {
-        }
-    };
-
-    // Create a trust manager that does not validate certificate chains
-    protected TrustManager[] trustAllCerts = new TrustManager[]{ x509TrustManager};
-
-    protected ConnectionSpec httpSpec;
-    protected OkHttpClient httpClient;
-
-    protected ConnectionSpec httpsCompatibleSpec;
-    protected OkHttpClient httpsCompatibleClient;
-
-    protected ConnectionSpec httpsStrongSpec;
-    protected OkHttpClient httpsStrongClient;
-
-    protected Server server;
-    protected HttpEnvelopeHandler httpHandler;
-
-    protected Proxy proxy = null;
-
-    protected Properties properties;
-
-    public ClearnetServerSensor(SensorsService sensorsService, Envelope.Sensitivity sensitivity, Integer priority) {
-        super(sensorsService, sensitivity, priority);
+    Byte registerHandler(AsynchronousHandler handler) {
+        handlers.put(nextHandlerId++, handler);
+        return nextHandlerId;
     }
 
     @Override
     public String[] getOperationEndsWith() {
-        return new String[]{".html"};
+        return new String[]{".html",".htm",".do",".json"};
     }
 
     @Override
@@ -93,226 +60,163 @@ public class ClearnetServerSensor extends BaseSensor {
 
     @Override
     public String[] getURLEndsWith() {
-        return new String[]{".html",".htm"};
+        return new String[]{".html",".htm",".do",".json"};
     }
 
     @Override
     public boolean send(Envelope e) {
-        URL url = e.getURL();
-        if(url != null) {
-            LOG.info("URL="+url.toString());
-        }
-        Map<String,Object> h = e.getHeaders();
-        Map<String,String> hStr = new HashMap<>();
-        if(h.containsKey(Envelope.HEADER_CONTENT_DISPOSITION)) {
-            hStr.put(Envelope.HEADER_CONTENT_DISPOSITION,(String)h.get(Envelope.HEADER_CONTENT_DISPOSITION));
-        }
-        if(h.containsKey(Envelope.HEADER_CONTENT_TYPE)) {
-            hStr.put(Envelope.HEADER_CONTENT_TYPE, (String)h.get(Envelope.HEADER_CONTENT_TYPE));
-        }
-        if(h.containsKey(Envelope.HEADER_CONTENT_TRANSFER_ENCODING)) {
-            hStr.put(Envelope.HEADER_CONTENT_TRANSFER_ENCODING, (String)h.get(Envelope.HEADER_CONTENT_TRANSFER_ENCODING));
-        }
-        if(h.containsKey(Envelope.HEADER_USER_AGENT)) {
-            hStr.put(Envelope.HEADER_USER_AGENT, (String)h.get(Envelope.HEADER_USER_AGENT));
-        }
-
-        ByteBuffer bodyBytes = null;
-        CacheControl cacheControl = null;
-        if(e.getMultipart() != null) {
-            // handle file upload
-            Multipart m = e.getMultipart();
-            hStr.put(Envelope.HEADER_CONTENT_TYPE, "multipart/form-data; boundary=" + m.getBoundary());
-            try {
-                bodyBytes = ByteBuffer.wrap(m.finish().getBytes());
-            } catch (IOException e1) {
-                e1.printStackTrace();
-                // TODO: Provide error message
-                LOG.warning("IOException caught while building HTTP body with multipart: "+e1.getLocalizedMessage());
-                return false;
-            }
-            cacheControl = new CacheControl.Builder().noCache().build();
-        }
-        Headers headers = Headers.of(hStr);
-
-        Message m = e.getMessage();
-        if(m instanceof DocumentMessage) {
-            Object contentObj = DLC.getContent(e);
-            if(contentObj instanceof String) {
-                if(bodyBytes == null) {
-                    bodyBytes = ByteBuffer.wrap(((String)contentObj).getBytes());
-                } else {
-                    bodyBytes.put(((String)contentObj).getBytes());
-                }
-            } else if(contentObj instanceof byte[]) {
-                if(bodyBytes == null) {
-                    bodyBytes = ByteBuffer.wrap((byte[])contentObj);
-                } else {
-                    bodyBytes.put((byte[])contentObj);
-                }
-            }
-        } else {
-            LOG.warning("Only DocumentMessages supported at this time.");
-            DLC.addErrorMessage("Only DocumentMessages supported at this time.",e);
-            return false;
-        }
-
-        RequestBody requestBody = null;
-        if(bodyBytes != null) {
-            requestBody = RequestBody.create(MediaType.parse((String) h.get(Envelope.HEADER_CONTENT_TYPE)), bodyBytes.array());
-        }
-
-        Request.Builder b = new Request.Builder().url(url);
-        if(cacheControl != null)
-            b = b.cacheControl(cacheControl);
-        b = b.headers(headers);
-        switch(e.getAction()) {
-            case ADD: {b = b.post(requestBody);break;}
-            case UPDATE: {b = b.put(requestBody);break;}
-            case REMOVE: {b = (requestBody == null ? b.delete() : b.delete(requestBody));break;}
-            case VIEW: {b = b.get();break;}
-            default: {
-                LOG.warning("Envelope.action must be set to ADD, UPDATE, REMOVE, or VIEW");
-                return false;
-            }
-        }
-        Request req = b.build();
-        if(req == null) {
-            LOG.warning("okhttp3 builder didn't build request.");
-            return false;
-        }
-        LOG.info("Sending http request, host="+url.getHost());
-        Response response = null;
-        if(url.toString().startsWith("https:")) {
-//            if(trustedHosts.contains(url.getHost())) {
-                try {
-//                    LOG.info("Trusted host, using compatible connection...");
-                    response = httpsCompatibleClient.newCall(req).execute();
-                    if(!response.isSuccessful()) {
-                        LOG.warning(response.toString());
-                        m.addErrorMessage(response.code()+"");
-                        return false;
-                    }
-                } catch (IOException e1) {
-                    m.addErrorMessage(e1.getLocalizedMessage());
-                    return false;
-                }
-//            } else {
-//                try {
-//                    System.out.println(ClearnetServerSensor.class.getSimpleName() + ": using strong connection...");
-//                    response = httpsStrongClient.newCall(req).execute();
-//                    if (!response.isSuccessful()) {
-//                        m.addErrorMessage(response.code()+"");
-//                        return false;
-//                    }
-//                } catch (IOException ex) {
-//                    ex.printStackTrace();
-//                    m.addErrorMessage(ex.getLocalizedMessage());
-//                    return false;
-//                }
-//            }
-        } else {
-            if(httpClient == null) {
-                LOG.severe("httpClient was not set up.");
-                return false;
-            }
-            try {
-                response = httpClient.newCall(req).execute();
-                if(!response.isSuccessful()) {
-                    m.addErrorMessage(response.code()+"");
-                    return false;
-                }
-            } catch (IOException e2) {
-                e2.printStackTrace();
-                m.addErrorMessage(e2.getLocalizedMessage());
-                return false;
-            }
-        }
-
-        LOG.info("Received http response.");
-        Headers responseHeaders = response.headers();
-        for (int i = 0; i < responseHeaders.size(); i++) {
-            LOG.info(responseHeaders.name(i) + ": " + responseHeaders.value(i));
-        }
-        ResponseBody responseBody = response.body();
-        if(responseBody != null) {
-            try {
-                DLC.addContent(responseBody.bytes(),e);
-            } catch (IOException e1) {
-                e1.printStackTrace();
-            } finally {
-                responseBody.close();
-            }
-            LOG.info(new String((byte[])DLC.getContent(e)));
-        } else {
-            LOG.info("Body was null.");
-            DLC.addContent(null,e);
+        if(!isTest)
+            sensorManager.sendToBus(e);
+        else {
+            // TODO: add content here
+            reply(e);
         }
 
         return true;
     }
 
-    void sendToBus(Envelope envelope) {
-        sensorsService.sendToBus(envelope);
-    }
-
     @Override
     public boolean reply(Envelope e) {
-        httpHandler.reply(e);
+        Byte handlerId = (Byte)e.getHeader(HANDLER_ID);
+        if(handlerId == null) {
+            LOG.warning("Handler id="+handlerId+" not found in Envelope header. Ensure this is placed in the Envelope header="+HANDLER_ID);
+            sensorManager.suspend(e);
+            return false;
+        }
+        AsynchronousHandler handler = handlers.get(handlerId);
+        if(handler == null) {
+            LOG.warning("Handler with id="+handlerId+" not registered. Please ensure it's registered prior to calling send().");
+            sensorManager.suspend(e);
+            return false;
+        }
+
         return true;
     }
 
     @Override
     public boolean start(Properties p) {
         LOG.info("Starting...");
-
         try {
             properties = Config.loadFromClasspath("clearnet-server.config", p, false);
         } catch (Exception e) {
             LOG.warning(e.getLocalizedMessage());
         }
 
-        ContextHandler context = new ContextHandler();
-        context.setContextPath( "/" );
-//        context.setContextPath(properties.getProperty("1m5.clearnet.server.path"));
-        context.setHandler( new HttpEnvelopeHandler(this) );
-
-        String host = "127.0.0.1";
-//            String hostProp = properties.getProperty(PROP_HTTP_SERVER_IP);
-//            if(hostProp != null && !"".equals(hostProp)) {
-//                host = hostProp;
-//            }
-        LOG.info("HTTP Server Host: "+host);
-
-        int port = 8080;
-//            String portStr = properties.getProperty(PROP_HTTP_SERVER_PORT);
-//            if(portStr != null) {
-//                port = Integer.parseInt(portStr);
-//            }
-        LOG.info("HTTP Server Port: "+port);
-
-        InetSocketAddress addr = new InetSocketAddress(host,port);
-        server = new Server(addr);
-        server.setHandler(context);
-
-        try {
-            server.start();
-        } catch (Exception e) {
-            e.printStackTrace();
+        if("true".equals(properties.getProperty(Config.PROP_UI))) {
+            // Start HTTP Server for 1M5 UI
+            AsynchronousHandler handler = new HttpEnvelopeHandler(this);
+            ContextHandler context = new ContextHandler();
+            context.setContextPath("/");
+            context.setHandler(handler);
+            boolean launchOnStart = "true".equals(properties.getProperty(Config.PROP_UI_LAUNCH_ON_START));
+            // 571 BC - Birth of Laozi, Chinese Philosopher and Writer, author of Tao Te Ching
+            if(!startServer("1M5", 571, context, launchOnStart))
+                return false;
         }
-        server.dumpStdErr();
-        try {
-            // The use of server.join() the will make the current thread join and
-            // wait until the server is done executing.
-            // See
-            // http://docs.oracle.com/javase/7/docs/api/java/lang/Thread.html#join()
-            server.join();
-        } catch (InterruptedException e) {
-            e.printStackTrace();
+
+        if(properties.getProperty(SERVERS_CONFIG)!=null) {
+            String serversConfig = properties.getProperty(SERVERS_CONFIG);
+            String[] servers = serversConfig.split("|");
+            for(String s : servers) {
+                String[] m = s.split(",");
+                String name = m[0];
+                if(name==null){
+                    LOG.warning("Name must be provided for HTTP server.");
+                    continue;
+                }
+                String portStr = m[1];
+                if(portStr==null){
+                    LOG.warning("Port must be provided for HTTP server with name="+name);
+                    continue;
+                }
+                int port = Integer.parseInt(portStr);
+                String handlerStr = m[2];
+                if(handlerStr==null){
+                    LOG.warning("Handler must be provided for HTTP server with name="+name);
+                    continue;
+                }
+                AsynchronousHandler handler = null;
+                try {
+                    handler = (AsynchronousHandler)Class.forName(handlerStr).newInstance();
+                } catch (InstantiationException e) {
+                    LOG.warning("Handler must be implementation of io.onemfive.clearnet.server.AsynchronousHandler");
+                    continue;
+                } catch (IllegalAccessException e) {
+                    LOG.warning("Getting an IllegalAccessException while attempting to instantiate Handler implementation class "+handlerStr+". Launch application with appropriate read access.");
+                    continue;
+                } catch (ClassNotFoundException e) {
+                    LOG.warning("Handler implementation "+handlerStr+" not found. Ensure library included.");
+                    continue;
+                }
+                String launchOnStartStr = m[3];
+                boolean launchOnStart = "true".equals(launchOnStartStr);
+
+                if(!startServer(name, port, handler, launchOnStart)) {
+                    LOG.warning("Unable to start server "+name);
+                }
+            }
         }
 
         LOG.info("Started.");
         return true;
+    }
+
+    public boolean startServer(String name, int port, Handler handler, boolean launch) {
+        Server server = new Server(new InetSocketAddress("127.0.0.1", port));
+        server.setHandler(handler);
+        try {
+            server.start();
+            servers.add(server);
+            LOG.info("HTTP Server for "+name+" UI started.\n    Host: 127.0.0.1\n    Port: "+port+"\n    Handler: "+handler.getClass().getName());
+        } catch (Exception e) {
+            LOG.warning("Exception caught while starting HTTP Server for 1M5 UI: "+e.getLocalizedMessage());
+            e.printStackTrace();
+            return false;
+        }
+//        server.dumpStdErr();
+//        try {
+//            The use of server.join() the will make the current thread join and
+//            wait until the server is done executing.
+//                    See
+//            http://docs.oracle.com/javase/7/docs/api/java/lang/Thread.html#join()
+//            server.join();
+//        } catch (InterruptedException e) {
+//            e.printStackTrace();
+//        }
+        if(launch)
+            launchBrowser("http://127.0.0.1:"+port+"/");
+        return true;
+    }
+
+    private void launchBrowser(String url) {
+        String[] cmd = null;
+        if(SystemVersion.isLinuxService()) {
+            LOG.info("OS is Linux.");
+            String[] browsers = { "epiphany", "firefox", "mozilla", "konqueror",
+                    "netscape", "opera", "links", "lynx" };
+            StringBuffer sb = new StringBuffer();
+            for (int i = 0; i < browsers.length; i++)
+                if(i == 0)
+                    sb.append(String.format(    "%s \"%s\"", browsers[i], url));
+                else
+                    sb.append(String.format(" || %s \"%s\"", browsers[i], url));
+            // If the first didn't work, try the next browser and so on
+            cmd = new String[]{"sh", "-c", sb.toString()};
+        } else if(SystemVersion.isMac()) {
+            LOG.info("OS is Mac.");
+            cmd = new String[]{"open " + url};
+        } else if(SystemVersion.isWindows()) {
+            LOG.info("OS is Windows.");
+            cmd = new String[]{"rundll32 url.dll,FileProtocolHandler " + url};
+        }
+
+        if(cmd != null) {
+            try {
+                Runtime.getRuntime().exec(cmd);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
     }
 
     @Override
@@ -332,7 +236,7 @@ public class ClearnetServerSensor extends BaseSensor {
 
     @Override
     public boolean shutdown() {
-        if(server != null) {
+        for(Server server : servers) {
             try {
                 server.stop();
             } catch (Exception e) {
@@ -345,6 +249,14 @@ public class ClearnetServerSensor extends BaseSensor {
     @Override
     public boolean gracefulShutdown() {
         return shutdown();
+    }
+
+    public static void main(String[] args) {
+        Properties p = new Properties();
+        p.setProperty("1m5.ui","true");
+        p.setProperty("1m5.ui.launchOnStart","true");
+        ClearnetServerSensor sensor = new ClearnetServerSensor(null, null, null);
+        sensor.start(p);
     }
 
 }
