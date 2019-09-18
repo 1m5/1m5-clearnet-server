d3bd71e2d2ce1e60e0d5afe7302e49ee91194923
objectorange <objectorange@1m5.io>
2019-09-18 08:48:09 -0400

    place in public util class

--- src/main/java/io/onemfive/clearnet/server/ClearnetServerSensor.java
+++ src/main/java/io/onemfive/clearnet/server/ClearnetServerSensor.java
@@ -386,51 +386,11 @@ public final class ClearnetServerSensor extends BaseSensor {
             return false;
         }
         if(launch)
-            launchBrowser("http://127.0.0.1:"+port+"/");
+            ClearnetServerUtil.launchBrowser("http://127.0.0.1:"+port+"/");
         return true;
     }
 
-    private void launchBrowser(String url) {
-        String[] cmd = null;
-        if(SystemVersion.isLinux()) {
-            LOG.info("OS is Linux.");
-            String[] browsers = { "purebrowser", "epiphany", "firefox", "mozilla", "konqueror",
-                    "netscape", "opera", "links", "lynx" };
-            StringBuffer sb = new StringBuffer();
-            for (int i = 0; i < browsers.length; i++)
-                if(i == 0)
-                    sb.append(String.format(    "%s \"%s\"", browsers[i], url));
-                else
-                    sb.append(String.format(" || %s \"%s\"", browsers[i], url));
-            // If the first didn't work, try the next browser and so on
-            cmd = new String[]{"sh", "-c", sb.toString()};
-        } else if(SystemVersion.isMac()) {
-            LOG.info("OS is Mac.");
-            cmd = new String[]{"open " + url};
-        } else if(SystemVersion.isWindows()) {
-            LOG.info("OS is Windows.");
-            try {
-				Desktop.getDesktop().browse(new URL(url).toURI());
-			} catch (MalformedURLException e) {
-                LOG.severe("MalformedURLException caught while launching browser for windows. Error message: "+e.getLocalizedMessage());
-			} catch (IOException e) {
-                LOG.severe("IOException caught while launching browser for windows. Error message: "+e.getLocalizedMessage());
-			} catch (URISyntaxException e) {
-                LOG.severe("URISyntaxException caught while launching browser for windows. Error message: "+e.getLocalizedMessage());
-			}
-            return;            
-        } else {
-            LOG.warning("Unable to determine OS therefore unable to launch a browser.");
-            return;
-        }
 
-        try {
-            Runtime.getRuntime().exec(cmd);
-        } catch (IOException e) {
-            LOG.warning(e.getLocalizedMessage());
-            e.printStackTrace();
-        }
-    }
 
     @Override
     public boolean pause() {
